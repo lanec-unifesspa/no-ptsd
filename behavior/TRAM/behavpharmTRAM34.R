0116e7ae80c5baf22bf587e556828987ffd62cf7
Laboratório de Neurociências e Comportamento "Frederico Guilherme Graeff <lanec.unifesspa@gmail.com>
2024-04-01 15:36:20 -0300

    Add files via upload

---
+++ behavior/TRAM/behavpharmTRAM34.R
@@ -0,0 +1,269 @@
+#Analysis scripts for data on the effects of TRAM-34 on time-dependent sensitization in zebrafish
+#Project: "Nitrergic signalling in an animal model for post-traumatic stress disorder"
+#Project coordinator: Monica Lima Maximino (ORCID: https://orcid.org/0000-0002-9816-3443)
+#Project grant: CNPq/Brazil (Process no. 423735/2016-0)
+
+#Install and load libraries
+if(!require(readr)){
+  install.packages("readr")
+  library(readr)
+}
+
+if(!require(RCurl)){
+  install.packages("RCurl")
+  library(RCurl)
+}
+
+if(!require(tidyverse)){
+  install.packages("tidyverse")
+  library(tidyverse)
+}
+
+if(!require(dabestr)){
+  devtools::install_github(repo = "ACCLAB/dabestr", ref = "dev")
+  library(dabestr)
+}
+
+#Load dataset for animals treated 30 min after CAS expsure
+TRAM34_30 <- read_csv("https://raw.githubusercontent.com/lanec-unifesspa/no-ptsd/main/behavior/TRAM/TRAM34_30.csv", col_types = cols(Treatment = col_factor(levels = c("CTRL", "CAS")), Drug = col_factor(levels = c("VEH", "TRAM34"))))
+View(TRAM34_30)
+
+#Reorganize data for plotting
+TRAM34_30_forplot <- TRAM34_30 %>%
+  unite(groups, c("Treatment", "Drug"))
+
+#Run two-way ANOVA on "Time on white", followed by Tukey's HSD post-hoc test
+anova_TRAM34_30_tw <- aov(formula = Time_on_white ~ Treatment*Drug, data = TRAM34_30)
+summary(anova_TRAM34_30_tw)
+TukeyHSD(anova_TRAM34_30_tw)
+
+#Create object for plotting
+multi_2group_TRAM34_30_tw <- load(TRAM34_30_forplot,
+                         x = groups, y = Time_on_white,
+                         idx = list(
+                           c("CTRL_VEH", "CAS_VEH"),
+                           c("CTRL_TRAM34", "CAS_TRAM34")
+                         )
+)
+
+#Plot
+multi_2group_TRAM34_30_tw %>%
+  mean_diff() %>%
+  dabest_plot()
+
+#Run two-way ANOVA on "Entries on white", followed by Tukey's HSD post-hoc test
+anova_TRAM34_30_en <- aov(formula = Entries ~ Treatment*Drug, data = TRAM34_30)
+summary(anova_TRAM34_30_en)
+TukeyHSD(anova_TRAM34_30_en)
+
+#Create object for plotting
+multi_2group_TRAM34_30_en <- load(TRAM34_30_forplot,
+                            x = groups, y = Entries,
+                            idx = list(
+                              c("CTRL_VEH", "CAS_VEH"),
+                              c("CTRL_TRAM34", "CAS_TRAM34")
+                            )
+)
+
+#Plot
+multi_2group_TRAM34_30_en %>%
+  mean_diff() %>%
+  dabest_plot()
+
+#Run two-way ANOVA on "Average entry duration", followed by Tukey's HSD post-hoc test
+anova_TRAM34_30_dur <- aov(formula = Duration ~ Treatment*Drug, data = TRAM34_30)
+summary(anova_TRAM34_30_dur)
+TukeyHSD(anova_TRAM34_30_dur)
+
+#Create object for plotting
+multi_2group_TRAM34_30_dur <- load(TRAM34_30_forplot,
+                            x = groups, y = Duration,
+                            idx = list(
+                              c("CTRL_VEH", "CAS_VEH"),
+                              c("CTRL_TRAM34", "CAS_TRAM34")
+                            )
+)
+
+#Plot
+multi_2group_TRAM34_30_dur %>%
+  mean_diff() %>%
+  dabest_plot()
+
+#Run two-way ANOVA on "Risk assessment", followed by Tukey's HSD post-hoc test
+anova_TRAM34_30_ra <- aov(formula = Risk_Assessment_N ~ Treatment*Drug, data = TRAM34_30)
+summary(anova_TRAM34_30_ra)
+TukeyHSD(anova_TRAM34_30_ra)
+
+#Create object for plotting
+multi_2group_TRAM34_30_ra <- load(TRAM34_30_forplot,
+                            x = groups, y = Risk_Assessment_N,
+                            idx = list(
+                              c("CTRL_VEH", "CAS_VEH"),
+                              c("CTRL_TRAM34", "CAS_TRAM34")
+                            )
+)
+
+#Plot
+multi_2group_TRAM34_30_ra %>%
+  mean_diff() %>%
+  dabest_plot()
+
+#Run two-way ANOVA on "Erratic swimming", followed by Tukey's HSD post-hoc test
+anova_TRAM34_30_es <- aov(formula = Erratic_swimming_N ~ Treatment*Drug, data = TRAM34_30)
+summary(anova_TRAM34_30_es)
+TukeyHSD(anova_TRAM34_30_es)
+
+#Create object for plotting
+multi_2group_TRAM34_30_es <- load(TRAM34_30_forplot,
+                            x = groups, y = Erratic_swimming_N,
+                            idx = list(
+                              c("CTRL_VEH", "CAS_VEH"),
+                              c("CTRL_TRAM34", "CAS_TRAM34")
+                            )
+)
+
+#Plot
+multi_2group_TRAM34_30_es %>%
+  mean_diff() %>%
+  dabest_plot()
+
+#Run two-way ANOVA on "Thigmotaxis", followed by Tukey's HSD post-hoc test
+anova_TRAM34_30_th <- aov(formula = Thigmotaxis_Percent ~ Treatment*Drug, data = TRAM34_30)
+summary(anova_TRAM34_30_th)
+TukeyHSD(anova_TRAM34_30_th)
+
+#Create object for plotting
+multi_2group_TRAM34_30_th <- load(TRAM34_30_forplot,
+                            x = groups, y = Thigmotaxis_Percent,
+                            idx = list(
+                              c("CTRL_VEH", "CAS_VEH"),
+                              c("CTRL_TRAM34", "CAS_TRAM34")
+                            )
+)
+
+#Plot
+multi_2group_TRAM34_30_th %>%
+  mean_diff() %>%
+  dabest_plot()
+
+#Load dataset for animals treated 90 min after CAS expsure
+TRAM34_90 <- read_csv("https://raw.githubusercontent.com/lanec-unifesspa/no-ptsd/main/behavior/TRAM/TRAM34_90.csv", col_types = cols(Treatment = col_factor(levels = c("CTRL", "CAS")), Drug = col_factor(levels = c("VEH", "TRAM34"))))
+View(TRAM34_90)
+
+#Reorganize data for plotting
+TRAM34_90_forplot <- TRAM34_90 %>%
+  unite(groups, c("Treatment", "Drug"))
+
+#Run two-way ANOVA on "Time on white", followed by Tukey's HSD post-hoc test
+anova_TRAM34_90_tw <- aov(formula = Time_on_white ~ Treatment*Drug, data = TRAM34_90)
+summary(anova_TRAM34_90_tw)
+TukeyHSD(anova_TRAM34_90_tw)
+
+#Create object for plotting
+multi_2group_TRAM34_90_tw <- load(TRAM34_90_forplot,
+                             x = groups, y = Time_on_white,
+                             idx = list(
+                               c("CTRL_VEH", "CAS_VEH"),
+                               c("CTRL_TRAM34", "CAS_TRAM34")
+                             )
+)
+
+#Plot
+multi_2group_TRAM34_90_tw %>%
+  mean_diff() %>%
+  dabest_plot()
+
+#Run two-way ANOVA on "Entries on white", followed by Tukey's HSD post-hoc test
+anova_TRAM34_90_en <- aov(formula = Entries ~ Treatment*Drug, data = TRAM34_90)
+summary(anova_TRAM34_90_en)
+TukeyHSD(anova_TRAM34_90_en)
+
+#Create object for plotting
+multi_2group_TRAM34_90_en <- load(TRAM34_90_forplot,
+                             x = groups, y = Entries,
+                             idx = list(
+                               c("CTRL_VEH", "CAS_VEH"),
+                               c("CTRL_TRAM34", "CAS_TRAM34")
+                             )
+)
+
+#Plot
+multi_2group_TRAM34_90_en %>%
+  mean_diff() %>%
+  dabest_plot()
+
+#Run two-way ANOVA on "Average entry duration", followed by Tukey's HSD post-hoc test
+anova_TRAM34_90_dur <- aov(formula = Duration ~ Treatment*Drug, data = TRAM34_90)
+summary(anova_TRAM34_90_dur)
+TukeyHSD(anova_TRAM34_90_dur)
+
+#Create object for plotting
+multi_2group_TRAM34_90_dur <- load(TRAM34_90_forplot,
+                              x = groups, y = Duration,
+                              idx = list(
+                                c("CTRL_VEH", "CAS_VEH"),
+                                c("CTRL_TRAM34", "CAS_TRAM34")
+                              )
+)
+
+#Plot
+multi_2group_TRAM34_90_dur %>%
+  mean_diff() %>%
+  dabest_plot()
+
+#Run two-way ANOVA on "Risk assessment", followed by Tukey's HSD post-hoc test
+anova_TRAM34_90_ra <- aov(formula = Risk_Assessment_N ~ Treatment*Drug, data = TRAM34_90)
+summary(anova_TRAM34_90_ra)
+TukeyHSD(anova_TRAM34_90_ra)
+
+#Create object for plotting
+multi_2group_TRAM34_90_ra <- load(TRAM34_90_forplot,
+                             x = groups, y = Risk_Assessment_N,
+                             idx = list(
+                               c("CTRL_VEH", "CAS_VEH"),
+                               c("CTRL_TRAM34", "CAS_TRAM34")
+                             )
+)
+
+#Plot
+multi_2group_TRAM34_90_ra %>%
+  mean_diff() %>%
+  dabest_plot()
+
+#Run two-way ANOVA on "Erratic swimming", followed by Tukey's HSD post-hoc test
+anova_TRAM34_90_es <- aov(formula = Erratic_swimming_N ~ Treatment*Drug, data = TRAM34_90)
+summary(anova_TRAM34_90_es)
+TukeyHSD(anova_TRAM34_90_es)
+
+#Create object for plotting
+multi_2group_TRAM34_90_es <- load(TRAM34_90_forplot,
+                             x = groups, y = Erratic_swimming_N,
+                             idx = list(
+                               c("CTRL_VEH", "CAS_VEH"),
+                               c("CTRL_TRAM34", "CAS_TRAM34")
+                             )
+)
+
+#Plot
+multi_2group_TRAM34_90_es %>%
+  mean_diff() %>%
+  dabest_plot()
+
+#Run two-way ANOVA on "Thigmotaxis", followed by Tukey's HSD post-hoc test
+anova_TRAM34_90_th <- aov(formula = Thigmotaxis_Percent ~ Treatment*Drug, data = TRAM34_90)
+summary(anova_TRAM34_90_th)
+TukeyHSD(anova_TRAM34_90_th)
+
+#Create object for plotting
+multi_2group_TRAM34_90_th <- load(TRAM34_90_forplot,
+                             x = groups, y = Thigmotaxis_Percent,
+                             idx = list(
+                               c("CTRL_VEH", "CAS_VEH"),
+                               c("CTRL_TRAM34", "CAS_TRAM34")
+                             )
+)
+
+#Plot
+multi_2group_TRAM34_90_th %>%
+  mean_diff() %>%
+  dabest_plot()
